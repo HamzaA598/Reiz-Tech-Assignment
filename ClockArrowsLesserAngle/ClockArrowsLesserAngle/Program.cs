a82334fd08e8455524fbfb732db76e7a76519db1
HamzaA598 <hamzaemad598@gmail.com>
2023-07-02 09:26:49 +0300

    Implemented CalculateLesserAngle

--- ClockArrowsLesserAngle/ClockArrowsLesserAngle/Program.cs
+++ ClockArrowsLesserAngle/ClockArrowsLesserAngle/Program.cs
@@ -16,6 +16,19 @@ namespace MyApp
         // each minute moves the hours arrow by some degree denoted by the following constant
         const double HOURS_ARROW_DEGREES_PER_MINUTE = HOURS_ARROW_DEGREES_PER_HOUR / MINUTES_PER_HOUR;
 
+        public static double CalculateLesserAngle(int hours, int minutes)
+        {
+            double minutesArrowDegree = minutes * MINUTES_ARROW_DEGREES_PER_MINUTE;
+            double hoursArrowDegree = hours * HOURS_ARROW_DEGREES_PER_HOUR + minutes * HOURS_ARROW_DEGREES_PER_MINUTE;
+
+            double lesserAngle = hoursArrowDegree - minutesArrowDegree;
+            if (lesserAngle < 0)
+                lesserAngle += DEGREES_PER_CYCLE;
+            lesserAngle = Math.Min(lesserAngle, DEGREES_PER_CYCLE - lesserAngle);
+
+            return lesserAngle;
+        }
+
         static void Main(string[] args)
         {
            
