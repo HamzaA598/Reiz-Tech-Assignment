21da0a51826006799006a4d9b82ae0c2e150f97d
HamzaA598 <hamzaemad598@gmail.com>
2023-07-02 08:41:20 +0300

    init Q1

--- ClockArrowsLesserAngle/ClockArrowsLesserAngle/Program.cs
+++ ClockArrowsLesserAngle/ClockArrowsLesserAngle/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace MyApp
+namespace MyApp 
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello World!");
         }
     }
 }
