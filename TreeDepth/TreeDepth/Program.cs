334a85a1d0ee0a14cfdef22485f2e49465db509e
HamzaA598 <hamzaemad598@gmail.com>
2023-07-02 10:21:15 +0300

    Add TreeDepth function

--- TreeDepth/TreeDepth/Program.cs
+++ TreeDepth/TreeDepth/Program.cs
@@ -2,11 +2,39 @@
 
 namespace MyApp
 {
+    // should the branch class be outside the Program class?
+    // should the inner branches list be public or returned using a getter?
+
     internal class Program
     {
+        public class Branch
+        {
+            public List<Branch> branches;
+        }
+
+        public static int TreeDepth(Branch current)
+        {
+            if (current.branches == null)
+                return 1;
+
+            int depth = 0;
+
+            foreach (Branch branch in current.branches)
+                depth = Math.Max(depth, TreeDepth(branch));
+
+            return 1 + depth;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Branch tree = new Branch();
+            tree.branches = new List<Branch>();
+            tree.branches.Add(new Branch());
+            tree.branches.Add(new Branch());
+            tree.branches.Add(new Branch());
+            tree.branches.Add(new Branch());
+            tree.branches[0].branches = new List<Branch>();
+            tree.branches[0].branches.Add(new Branch());
+            Console.WriteLine(TreeDepth(tree));
         }
     }
 }
