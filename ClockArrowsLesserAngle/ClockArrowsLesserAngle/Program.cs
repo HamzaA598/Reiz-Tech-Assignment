47275ed22fe948e2bbfddae8e8d96cb6fa127b13
HamzaA598 <hamzaemad598@gmail.com>
2023-07-02 09:25:21 +0300

    init constants

--- ClockArrowsLesserAngle/ClockArrowsLesserAngle/Program.cs
+++ ClockArrowsLesserAngle/ClockArrowsLesserAngle/Program.cs
@@ -4,9 +4,21 @@ namespace MyApp
 {
     internal class Program
     {
+
+        const double MINUTES_PER_HOUR = 60;
+        const double HOURS_PER_CYCLE = 12;
+        // there are 360 degrees around the center of the clock, degree 0 is the 12 clock mark
+        const double DEGREES_PER_CYCLE = 360;
+
+        const double MINUTES_ARROW_DEGREES_PER_MINUTE = DEGREES_PER_CYCLE / MINUTES_PER_HOUR;
+
+        const double HOURS_ARROW_DEGREES_PER_HOUR = DEGREES_PER_CYCLE / HOURS_PER_CYCLE;
+        // each minute moves the hours arrow by some degree denoted by the following constant
+        const double HOURS_ARROW_DEGREES_PER_MINUTE = HOURS_ARROW_DEGREES_PER_HOUR / MINUTES_PER_HOUR;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
